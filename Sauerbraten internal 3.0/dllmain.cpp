d497718573e4fd166ae9159cff99ad69155e325e
Ashencursor <167921826+Ashencursor@users.noreply.github.com>
2024-09-04 16:49:43 -0400

    GUI update

--- Sauerbraten internal 3.0/dllmain.cpp
+++ Sauerbraten internal 3.0/dllmain.cpp
@@ -79,19 +79,22 @@ namespace Hook {
 
     LRESULT WINAPI hkWndProc(HWND hWnd, UINT umsg, WPARAM wParam, LPARAM lParam) {
 
+        // Must be up here to correctly process events
         if (GUI::isActive) {
-
             //This will allow things to interact with the imgui menu, so if my cursor from tabbing out of the game closes the menu then it closes
             if (ImGui_ImplWin32_WndProcHandler(hWnd, umsg, wParam, lParam))
                 return true;
+        }
 
+        if (GUI::isActive) {
+           
             // Ensure the cursor is visible
-            //ShowCursor(TRUE);
-            ImGui::GetIO().MouseDrawCursor = true;
-            Hook::SDL_RelativeMouseModePtr(0);
-
+            ShowCursor(TRUE);
+            //ImGui::GetIO().MouseDrawCursor = true;// Use to draw mouse
+            Hook::SDL_RelativeMouseModePtr(0);// Set Mouse free from center of the screen
 
             // Handle mouse input differently or ignore it for the game
+            // Can stop the in game mouse from moving
             if (umsg == WM_MOUSEMOVE || umsg == WM_LBUTTONDOWN || umsg == WM_RBUTTONDOWN) {
                 // Let ImGui handle the mouse input instead of the game
                 return 0; // Return 0 to stop the game from handling the input
@@ -100,7 +103,11 @@ namespace Hook {
         else {
             // Hide the cursor when the menu is closed
             //ShowCursor(FALSE);
-            ImGui::GetIO().MouseDrawCursor = false;
+            //ImGui::GetIO().MouseDrawCursor = false;
+        }
+
+        switch (umsg) {
+
         }
 
         // Call OriginalWndProc
